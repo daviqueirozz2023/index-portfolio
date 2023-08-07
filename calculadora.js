abeaa427245ee71290e451d23e85b5e53d32b604
daviqueirozz2023 <134320097+daviqueirozz2023@users.noreply.github.com>
2023-08-07 16:50:07 -0300

    Create calculadora.js

---
+++ calculadora.js
@@ -0,0 +1,26 @@
+function insert(num){
+
+    var numero = window.document.getElementById('resultado').innerHTML;
+    window.document.getElementById('resultado').innerHTML = numero + num;
+
+}
+
+function clean() {
+   window.document.getElementById('resultado').innerHTML = ""
+}
+
+function back(){
+
+   var resultado = window.document.getElementById('resultado').innerHTML;
+   window.document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1)
+}
+
+function calcular() {
+
+   var resultado = document.getElementById('resultado').innerHTML;
+   if(resultado) {
+       document.getElementById('resultado').innerHTML = eval(resultado);
+   }else{
+       document.getElementById('resultado').innerHTML = "nada..."
+   }
+}
